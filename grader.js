f4e2ae609f9f9c642350c79ce19908cdecc45161
Chris Sinco <csi7585@gmail.com>
2013-07-17 23:42:31 -0700

    Added more checks to JSON file, modified grader.js to take URL as input

--- grader.js
+++ grader.js
@@ -24,6 +24,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -55,6 +57,31 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var loadUrl = function(urlpath) {
+  rest.get(urlpath).on('complete', checkUrl);
+}
+
+var checkUrl = function(result, response) {
+  if (result instanceof Error) {
+    console.error('URL error: ' + util.format(result));
+    process.exit(1);
+  } else {
+    $ = cheerio.load(result);
+    var checks = loadChecks(program.checks).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    outputChecks(out);
+  }
+};
+
+var outputChecks = function(cj) {
+  var oj = JSON.stringify(cj, null, 4);
+  console.log(oj);
+}
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +92,14 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Path to file on web')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+      loadUrl(program.url);
+    } else {
+      var checkJson = checkHtmlFile(program.file, program.checks);
+      outputChecks(checkJson); 
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
